8fb8725a47a485ed9e4d4b383f31febb1ea7e488
Vladimir Roguljic <vladimir.roguljic@gauss.hr>
2019-09-07 11:51:28 +0200

    adding material, adding chatservice, adding sent message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ChatService} from "./chat.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  title = 'ChatApp';
+  message:string;
+  messages: string[] = [];
+  constructor(private chat: ChatService) {}
+
+  sendMessage() {
+    this.chat.sendMessage(this.message);
+    this.message = '';
+  }
+
+
+  ngOnInit() {
+    this.chat.getMessages()
+      .subscribe((message: string) => {
+        this.messages.push(message);
+      });
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import {ChatService} from "./chat.service";
+import {FormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatTableModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [ChatService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
